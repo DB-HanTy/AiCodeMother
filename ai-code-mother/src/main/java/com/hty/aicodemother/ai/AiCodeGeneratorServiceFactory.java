6c4f26939b046894709a081a0787aef4839f980c
Hty <15161656368@163.com>
2026-03-09 15:06:10 +0800

    Ai生成应用（1-实现应用生成）

---
+++ ai-code-mother/src/main/java/com/hty/aicodemother/ai/AiCodeGeneratorServiceFactory.java
@@ -0,0 +1,27 @@
+package com.hty.aicodemother.ai;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 创建 Ai服务创建工厂
+ */
+@Configuration
+public class AiCodeGeneratorServiceFactory {
+
+    @Resource
+    private ChatModel chatModel;
+
+    /**
+     * 创建 Ai代码生成器服务
+     *
+     * @return
+     */
+    @Bean
+    public AiCodeGeneratorService aiCodeGeneratorService() {
+        return AiServices.create(AiCodeGeneratorService.class, chatModel);
+    }
+}
