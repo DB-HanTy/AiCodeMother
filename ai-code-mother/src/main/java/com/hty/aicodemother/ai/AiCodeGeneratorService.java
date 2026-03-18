55cbc658aca42345fee9f5c0e0033d2e5a533ba9
Hty <15161656368@163.com>
2026-03-18 12:43:26 +0800

    对话历史模块（工程项目生成）

--- ai-code-mother/src/main/java/com/hty/aicodemother/ai/AiCodeGeneratorService.java
+++ ai-code-mother/src/main/java/com/hty/aicodemother/ai/AiCodeGeneratorService.java
@@ -2,7 +2,9 @@ package com.hty.aicodemother.ai;
 
 import com.hty.aicodemother.ai.model.HtmlCodeResult;
 import com.hty.aicodemother.ai.model.MultiFileCodeResult;
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
 import reactor.core.publisher.Flux;
 
 /**
@@ -35,7 +37,7 @@ public interface AiCodeGeneratorService {
      * @return 生成的代码结果
      */
     @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
-    Flux<String> generateHtmlCodeStream(String userMessage);
+    Flux<String> generateHtmlCodeStream(@UserMessage String userMessage);
 
     /**
      * 生成多文件代码
@@ -44,7 +46,17 @@ public interface AiCodeGeneratorService {
      * @return 生成的代码结果
      */
     @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
-    Flux<String> generateMultiFileCodeStream(String userMessage);
+    Flux<String> generateMultiFileCodeStream(@UserMessage String userMessage);
+
+    /**
+     * 生成 Vue 项目代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成过程的流式响应
+     */
+    @SystemMessage(fromResource = "prompt/codegen-vue-project-system-prompt.txt")
+    Flux<String> generateVueProjectCodeStream(@MemoryId long appId, @UserMessage String userMessage);
+
 }
 
 
