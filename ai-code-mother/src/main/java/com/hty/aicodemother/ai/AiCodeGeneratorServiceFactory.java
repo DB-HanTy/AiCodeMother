b4f9e19afcef1b1fd982894b600a33d548d9174b
Hty <15161656368@163.com>
2026-04-01 15:23:22 +0800

    系统优化（稳定性优化）

--- ai-code-mother/src/main/java/com/hty/aicodemother/ai/AiCodeGeneratorServiceFactory.java
+++ ai-code-mother/src/main/java/com/hty/aicodemother/ai/AiCodeGeneratorServiceFactory.java
@@ -3,6 +3,7 @@ package com.hty.aicodemother.ai;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.hty.aicodemother.ai.guardrail.PromptSafetyInputGuardrail;
+import com.hty.aicodemother.ai.guardrail.RetryOutputGuardrail;
 import com.hty.aicodemother.ai.tools.*;
 import com.hty.aicodemother.exception.BusinessException;
 import com.hty.aicodemother.exception.ErrorCode;
@@ -101,7 +102,9 @@ public class AiCodeGeneratorServiceFactory {
                         .hallucinatedToolNameStrategy(toolExecutionRequest -> ToolExecutionResultMessage.from(
                                 toolExecutionRequest, "Error: there is no tool called " + toolExecutionRequest.name()
                         ))
+                        .maxSequentialToolsInvocations(20) // 最多连续调用20次工具
                         .inputGuardrails(new PromptSafetyInputGuardrail()) // 添加输入护轨
+                        .outputGuardrails(new RetryOutputGuardrail())// 添加输出护轨
                         .build();
             }
             case HTML, MULTI_FILE -> {
@@ -112,6 +115,7 @@ public class AiCodeGeneratorServiceFactory {
                         .streamingChatModel(openAiStreamingChatModel)
                         .chatMemory(chatMemory)
                         .inputGuardrails(new PromptSafetyInputGuardrail()) // 添加输入护轨
+                        .outputGuardrails(new RetryOutputGuardrail())// 添加输出护轨
                         .build();
             }
             default -> throw new BusinessException(ErrorCode.SYSTEM_ERROR,
